b6dcaba9a69e9c6ea041cbdc03de4b6c64213bae
amar <amar.pandey@mindtree.com>
2024-04-12 16:21:26 +0530

    introducing login token call

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -7,36 +7,33 @@ const getVehicleServices = async (sidToken) =>{
     try {
       console.log(`welcome to get vehicle service`);
       // return 'hello';
-      try{
-        const res = await axios({
-            method: 'POST',
-            url:'https://hst-api.wialon.com/wialon/ajax.html',
-            params:{
-                svc: 'token/login',
-                params: '{"token":"9e582221ad39b510b1c6951d6df5a2a40513B3847580C9B771D0B2EB25462F5D2BBAF337","operateAs":"","appName":"","checkService":""}'
-            }
-        });
-        // console.log('Hey wialon');
-        // console.log(res.data.eid);
-        let token = res.data.eid;
-        return token;
-      } catch (error) {
-        console.error(error); // `error` will be whatever you passed to `reject()` at the top
-        return error;
-      }
+      
+      const res = await axios({
+          method: 'POST',
+          url:'https://hst-api.wialon.com/wialon/ajax.html',
+          params:{
+              svc: 'token/login',
+              params: '{"token":"9e582221ad39b510b1c6951d6df5a2a40513B3847580C9B771D0B2EB25462F5D2BBAF337","operateAs":"","appName":"","checkService":""}'
+          }
+      });
+      // console.log('Hey wialon');
+      // console.log(res.data.eid);
+      let token = res.data.eid;
+      // return token;
+      
 
-        // Making vehicle detail call
-        // const vehicleDetails = await axios({
-        //     method: 'get',
-        //     url:'https://hst-api.wialon.com/wialon/ajax.html',
-        //     params:{
-        //         svc: 'core/update_data_flags',
-        //         params: '{"spec":[{"type":"type","data":"avl_unit","flags":5121,"mode":0}]}',
-        //         sid: token
-        //     }
-        // });
-        // const vehicleData = vehicleDetails.data;
-        // return vehicleData;
+      // Making vehicle detail call
+      const vehicleDetails = await axios({
+          method: 'get',
+          url:'https://hst-api.wialon.com/wialon/ajax.html',
+          params:{
+              svc: 'core/update_data_flags',
+              params: '{"spec":[{"type":"type","data":"avl_unit","flags":5121,"mode":0}]}',
+              sid: token
+          }
+      });
+      const vehicleData = vehicleDetails.data;
+      return vehicleData;
 
       } catch (error) {
         console.error(error); // `error` will be whatever you passed to `reject()` at the top
