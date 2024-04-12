47bb76684eca8b56f01f536b4c2cac612f206ed7
amar <amar.pandey@mindtree.com>
2024-04-12 13:23:10 +0530

    introducing login token call

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -7,25 +7,25 @@ const getVehicleServices = async (sidToken) =>{
             method: 'get',
             url:'https://hst-api.wialon.com/wialon/ajax.html',
             params:{
-                svc: 'core/create_auth_hash',
-                params: '{}',
-                sid: sidToken
+                svc: 'token/login',
+                params: '{"token":"9e582221ad39b510b1c6951d6df5a2a40513B3847580C9B771D0B2EB25462F5D2BBAF337","operateAs":"","appName":"","checkService":""}'
             }
         });
-        let token = res.data.authHash;
+        let token = res;
+        return token;
 
         // Making vehicle detail call
-        const vehicleDetails = await axios({
-            method: 'get',
-            url:'https://hst-api.wialon.com/wialon/ajax.html',
-            params:{
-                svc: 'core/update_data_flags',
-                params: '{"spec":[{"type":"type","data":"avl_unit","flags":5121,"mode":0}]}',
-                sid: token
-            }
-        });
-        const vehicleData = vehicleDetails.data;
-        return vehicleData;
+        // const vehicleDetails = await axios({
+        //     method: 'get',
+        //     url:'https://hst-api.wialon.com/wialon/ajax.html',
+        //     params:{
+        //         svc: 'core/update_data_flags',
+        //         params: '{"spec":[{"type":"type","data":"avl_unit","flags":5121,"mode":0}]}',
+        //         sid: token
+        //     }
+        // });
+        // const vehicleData = vehicleDetails.data;
+        // return vehicleData;
 
       } catch (error) {
         console.error(error); // `error` will be whatever you passed to `reject()` at the top
