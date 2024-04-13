561385f2ccdd1056f029c14145666a3aa1ec8e5a
amar <amar.pandey@mindtree.com>
2024-04-13 11:16:17 +0530

    fetching vehicle result rows

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -1,8 +1,13 @@
 
 const axios = require('axios');
+
+
+
 const getVehicleServices = async (sidToken) =>{
     try {
       console.log(`welcome to get vehicle service`);
+      // return 'hello';
+      
       const res = await axios({
           method: 'POST',
           url:'https://hst-api.wialon.com/wialon/ajax.html',
@@ -11,11 +16,14 @@ const getVehicleServices = async (sidToken) =>{
               params: '{"token":"9e582221ad39b510b1c6951d6df5a2a40513B3847580C9B771D0B2EB25462F5D2BBAF337","operateAs":"","appName":"","checkService":""}'
           }
       });
+      // console.log('Hey wialon');
+      // console.log(res.data.eid);
       let token = res.data.eid;
+      // return token;
+      
 
       // Making vehicle detail call
-      console.log(`welcome to get vehicle details`);
-      const groupVehicleData = await axios({
+      const vehicleDetails = await axios({
           method: 'get',
           url:'https://hst-api.wialon.com/wialon/ajax.html',
           params:{
@@ -26,18 +34,7 @@ const getVehicleServices = async (sidToken) =>{
       });
 
       // Fetching result rows
-      console.log(`welcome to get result rows`);
-      // const vehicleReportData = await axios({
-      //     method: 'get',
-      //     url:'https://hst-api.wialon.com/wialon/ajax.html',
-      //     params:{
-      //         svc: 'report/get_result_rows',
-      //         params: '{"tableIndex":0,"indexFrom":0,"indexTo":3}',
-      //         sid: token
-      //     }
-      // });
-
-      const vehicleData = groupVehicleData;
+      const vehicleData = vehicleDetails.data;
       return vehicleData;
 
       } catch (error) {
