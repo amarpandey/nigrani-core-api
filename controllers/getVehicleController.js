77d0865933dd17c05e48a8c5c477e3525a773914
amar <amar.pandey@mindtree.com>
2024-04-19 10:18:48 +0530

    more vehicle groups added

--- controllers/getVehicleController.js
+++ controllers/getVehicleController.js
@@ -7,6 +7,7 @@ const getVehicleDetails = async (req, res)=>{
         // Calling getVehicle service 
         const userToken = req.query.token;
         const reportFrom = (new Date(req.query.from).getTime() / 1000);
+        console.log((new Date(req.query.to).setHours(23, 59, 59, 999)));
         const reportTo = new Date((new Date(req.query.to).setHours(23, 59, 59, 999)).getTime() / 1000);
         console.log('token :: '+ userToken);
         console.log('from :: '+ reportFrom);
