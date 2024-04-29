a843db8bc3fbd52afabaad3a7e44dd41a1297c74
amar <amar.pandey@mindtree.com>
2024-04-29 14:36:53 +0530

    multi report support

--- services/getVehicleService.js
+++ services/getVehicleService.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 const getVehicleServices = async (sidToken, reportFrom, reportTo, reportType) =>{
     try {
-        console.log(`welcome to get vehicle service ${reportFrom} , ${reportTo}`, template);
+        console.log(`welcome to get vehicle service ${reportFrom} , ${reportTo}`);
         // return 'hello';
         
         const res = await axios({
